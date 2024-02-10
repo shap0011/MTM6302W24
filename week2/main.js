86cdf8dbce612d6eadbc8f4fa615db24439d2d6c
Olga Durham <shap0011@algonquinlive.com>
2024-02-09 21:38:20 -0500

    week 2

---
+++ week2/main.js
@@ -0,0 +1,57 @@
+// Declaring without assignment
+//var employeeName;
+
+// Redeclaring  variable
+//var employeeName = "John";
+
+// Reassigning value to variable
+//employeeName = "James";
+
+// Declaring without assignment
+//let employeeName;
+
+// Redeclaring  variable
+//let employeeName = "John"; // Error
+
+// Reassigning value to variable
+// employeeName = "James";
+
+// Declaring without assignment
+// const employeeName; // Error!
+
+// Redeclaring  variable
+// const employeeName = "John"; // Error
+
+// Declaring  variable
+// const employeeName = "John"; 
+
+// Reassigning value to variable
+// employeeName = "James"; // Error
+
+// const animal = 'dog'
+
+// const phone = "613-123-4567"
+
+// Declare variables
+// const pet = "cat";
+// const numberOfPets = 2;
+
+// Concatenate variables within a string
+// console.log("I have " + numberOfPets + " " + pet + "s.");
+// Template literal
+// console.log(`I have ${numberOfPets} ${pet}s.`);
+
+// Numbers
+// let integerExample = 100;
+// let negativeExample = -100;
+// let decimalExample = 100.5;
+// let scientificExample = 1e3;
+
+// Booleans
+// let isActive = true;
+// let isAdmin = false;
+
+
+
+
+
