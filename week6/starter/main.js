bf750e6bba8315d2ebdbbdb0873311cc4932727d
platard <platard@algonquincollege.com>
2024-02-12 21:33:19 -0500

    add week6 starter files & week5 final

---
+++ week6/starter/main.js
@@ -0,0 +1,73 @@
+/*** Inserting elements with innertHTML */
+
+// Use the 'courses' array from script.js to create a navigation bar that displays the first two course names. Save the the resulting HTML template in the variable 'courseNav'
+const courseNav = ``
+
+// Display the template in the document using the DOM element with the ID 'list'.
+
+
+// Add the third course to the list.  Ensuring to retain all previously written code.
+
+
+//Dinamically create, and display in the document, all the "credentials". Use the 'students' array, and a 'for loop'.
+// 1- Retrieve the DOM element
+// 2- create the template 
+// 3- insert the template
+
+
+//Display all the "Credentials" using the innerHTML only once. Use an array to save multiple templates and the join() method.
+
+
+//Use the same approach but instead of innerHTML() use insertAdjacentHTML() to display the courses
+
+
+
+
+/*** Create HMTL element with createElement() method */
+//Retrieve the DOM element that has the id 'student-table'
+
+
+
+//Declare a function to create a table. No parameters are needed.
+
+    // Function body: Using a for loop, create the rows/cells and fill them with the student's name, number, and email.
+    //for(let i=0; i<students.length; i++) { 
+
+        // Create a new 'table row' for the contact 
+        // const tableRow = document.createElement('tr')
+            
+        // Create a new cell for the Name
+        // const tableCellName =  
+        
+        // Fill the value of the cell 
+        // tableCellName.textContent = 
+        
+        // Append the cell in the row element. Use the method appendChild()
+        // _row_.appendChild(_cell_)
+
+        // Repeat the process to insert the StudentNumber and Email cells
+        
+        // Append the row in the table container ''student-table'' 
+      
+
+    // } // Closes the for loop
+// } // Closes the function
+
+//Call the function to display the table in the page
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
