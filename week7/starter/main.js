d02679d73293c760e0420af3b3b30d199e3951fd
platard <platard@algonquincollege.com>
2024-02-19 21:17:26 -0500

    add wek 7 starter files

---
+++ week7/starter/main.js
@@ -0,0 +1,43 @@
+/*** Object literals ***/
+
+//Create your HTML file, link your JS file, and install Bootstrap.
+
+//Create an array of objects (data structure) to manage store items. Use two properties (productName, price) in the object. Create two products.
+
+//Print the first product in the console; retrieve the 'productName' property.
+
+//Add one extra item to the 'products' array using a method
+
+//Update the price of one of the products
+
+//Add the property 'size' to any of the products
+
+const descriptions = [
+    'First product description',
+    'Second product description',
+    'Third product description'
+]
+//Add the property 'description' to all the products(objects); use the 'descriptions' array
+
+//Delete the property 'size'
+
+//Go back to the object definition and create a method(function), as a property, to update the 'price' of a product. Test it in the console.
+
+//Do the same with a regular function
+    //1.declare the function; identify the inputs that you need.
+   
+        //2.find the right product
+        
+        //3.assign the new value
+      
+
+
+//Display all the products in the 'document'. 
+
+    //1.define varibles; 'DOM element' and a empty array
+
+    //2.define your 'html template'; push 'html code'(template) to the array.
+
+    //3.join and insert the complete 'html template'
+ 
+
