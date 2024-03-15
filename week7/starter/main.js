8328e7c9677b78130601b79367eeb6bf13cc2642
Olga Durham <shap0011@algonquinlive.com>
2024-03-15 09:30:23 -0400

    adds codes

--- week7/starter/main.js
+++ week7/starter/main.js
@@ -3,38 +3,65 @@
 //Create your HTML file, link your JS file, and install Bootstrap.
 
 //Create an array of objects (data structure) to manage store items. Use two properties (productName, price) in the object. Create two products.
-
+const products = [
+    {
+        productName: 'laptop',
+        price: 1000,
+        updatePrice: function(newPrice) {
+            this.price = newPrice
+        }
+    },
+    {
+        productName: 'tablet',
+        price: 500
+    }
+]
 //Print the first product in the console; retrieve the 'productName' property.
-
+console.log(products[0].productName)
 //Add one extra item to the 'products' array using a method
-
+products.push({productName:'phone', price: 300})
+console.log(products)
 //Update the price of one of the products
-
+//products[0]['price'] = 600
+products[0].price = 600 // most common practice
 //Add the property 'size' to any of the products
-
+products[0].size = '15 inches'
 const descriptions = [
     'First product description',
     'Second product description',
     'Third product description'
 ]
 //Add the property 'description' to all the products(objects); use the 'descriptions' array
-
+for(let i = 0; i < products.length; i++){
+    products[i].description = descriptions[i]
+}
 //Delete the property 'size'
-
+delete products[0].size
 //Go back to the object definition and create a method(function), as a property, to update the 'price' of a product. Test it in the console.
-
+//products[0].updatePrice(1000)
+products[1].updatePrice = function
+(newPrice){
+    this.price = newPrice
+}
 //Do the same with a regular function
     //1.declare the function; identify the inputs that you need.
-   
+   function updatePrice(product, newPrice) {
         //2.find the right product
-        
+        // const foundProduct = products.find(function(prod){
+        //     return prod.productName === product
+        // })
+        // const foundProduct = products.find(prod => prod.productName === product)
+        const foundProduct = products.find(productObj => productObj.productName === product)
+
         //3.assign the new value
-      
+        foundProduct.price = newPrice
+    } 
+    updatePrice('tablet', 450)
 
 
 //Display all the products in the 'document'. 
 
-    //1.define varibles; 'DOM element' and a empty array
+    //1.define variables; 'DOM element' and a empty array
 
     //2.define your 'html template'; push 'html code'(template) to the array.
 
--- week9/starter/main.js
+++ week9/starter/main.js
@@ -1,36 +1,66 @@
 /*** Event listener ***/
 
 // 1.Display the ID of the last student in the list.
-const $studentList = document.getElementById('studentList')
+const $studentList = document.getElementById("studentList");
+
+function displayStudentId() {
+//   $studentList.innerHTML = `<div class=""`;
 
 $studentList.innerHTML = `<div class="col-md-6">
     <div class="ac-card">
         <div class="ac-card-image d-flex justify-content-center align-items-center"><i class="bi bi-person-bounding-box display-1"></i></div>
-        <div class="ac-card-info">
-            <p>
-            <strong>${students[0].name}</strong><br>
-            <span>${students[0].studentNumber}</span>-
-            <span>${students[0].userName}</span>
-            </p>
-            <p>${students[0].email}</p>
-            <!-- here the button -->
-            <button type="button" class="btn btn-primary">Update</button>
+            <div class="ac-card-info">
+                <p>
+                <strong>${students[students.length - 1].name}</strong><br>
+                <span>${students[students.length - 1].studentNumber}</span>-
+                <span>${students[students.length - 1].userName}</span>
+                </p>
+                <p>${students[0].email}</p>
+                <!--<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"> -->
+                <button type="button" class="btn btn-primary">
+                Launch demo modal
+                </button>
+                <!-- <button type="button" class="btn btn-primary">Update</button> --> 
            
-        </div>
-        <div class="ac-card-footer">
-            <img class="ac-logo" src="https://imdac.github.io/images/codepen/AC_WORDMARK_1C_WHT.png">
+            </div>
+            <div class="ac-card-footer">
+                <img class="ac-logo" src="https://imdac.github.io/images/codepen/AC_WORDMARK_1C_WHT.png">
+            </div>
         </div>
     </div>
-    </div>
-    `
-// 2.Create a button in the student ID. Use the Bootstrap modal.
+    `;
+}
+
+const $button = document.querySelector("button");
+$button.addEventListener("mouseover", function () {
+  console.log("click");
+  $button.setAttribute("data-bs-toggle", "modal");
+  $button.setAttribute("data-bs-target", "#exampleModal");
+});
 
 // 3.Add an event listener to the button.
-
 
 /*** Form ***/
 // 5.Add an event listener to the 'update form' to allow updating of the student's information.
 
+const $updateForm = document.getElementById("updateForm");
+$updateForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log($updateForm.elements);
+
+  for (const input of $updateForm.elements) {
+    // console.log(input.name)
+    console.log(input.value);
+  }
+
+  console.log(document.getElementById("inputUsername").value);
+
+  const inputUsername = document.getElementById("inputUsername").value; // without $ sign it's just a variable, not html
+  // 2. Update the student's information
+  students[students.length - 1].userName =
+    // 3. Display the updated student's information
+    displayStudentId();
+});
 
 /*** Filter ***/
 // 6.Add an event listener to the 'Search' form. Test it with both 'submit' and 'input' events to display a filtered list based on the student's name.
